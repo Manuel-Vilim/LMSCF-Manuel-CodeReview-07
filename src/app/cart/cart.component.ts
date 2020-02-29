0affe78b9c3761078c06ee64c167b032a77190b4
Manuel Vilim <Manuel.Vilim@gmx.at>
2020-02-29 18:27:26 +0100

    cr7

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -6,45 +6,11 @@ import { FormBuilder } from '@angular/forms';
   templateUrl: './cart.component.html',
   styleUrls: ['./cart.component.css']
 })
-
 export class CartComponent implements OnInit {
-  product;
-  checkoutForm;
-    constructor(private cartService: CartService, private formBuilder: FormBuilder) {
-      this.checkoutForm = this.formBuilder.group({
-      name: '',
-      address: ''
-    }); }
-    ngOnInit() {
-      this.product = this.cartService.getproduct();
-    }
-      onSubmit(customerData) {
-        // Process checkout data here
-        console.warn('Your order has been submitted', customerData);
-        this.product = this.cartService.clearCart();
-        this.checkoutForm.reset(alert('thank you for your purchase'));
-      }
-      plus(i: string | number) {
-        this.product[i].quantity++;
-        this.product[i].total = this.product[i].quantity * this.product[i].price;
-    }
-      minus(i: string | number) {
-        if (this.product[i].quantity === 1) {
-          this.product[i].quantity = 0;
-          this.product.splice(i, 1);
-        } else {
-          this.product[i].quantity--;
-        }
-      }
-      removeItem(i: string | number) {
-        this.product[i].quantity = 0;
-        this.product.splice(i, 1 );
-      }
-      total() {
-        let total = 0;
-        for (let i of this.product) {
-          total += i.total;
-          console.log(total);
-        }
-      }
+
+  constructor() { }
+
+  ngOnInit(): void {
   }
+
+}
