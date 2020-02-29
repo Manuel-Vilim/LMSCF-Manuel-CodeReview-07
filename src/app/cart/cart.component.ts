cd59b6767c7c3d92ce349e1477c1ef2d6c110197
Manuel Vilim <Manuel.Vilim@gmx.at>
2020-02-29 18:56:38 +0100

    cr7

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -14,3 +14,57 @@ export class CartComponent implements OnInit {
   }
 
 }
+
+
+/* That Part isn´t working*//*
+import { Component, OnInit } from '@angular/core';
+import { CartService } from '../cart.service';
+import { FormBuilder } from '@angular/forms';
+
+@Component({
+  selector: 'app-cart',
+  templateUrl: './cart.component.html',
+  styleUrls: ['./cart.component.css']
+})
+export class CartComponent implements OnInit {
+  items;
+  checkoutForm;
+    constructor(private cartService: CartService, private formBuilder: FormBuilder) {
+      this.checkoutForm = this.formBuilder.group({
+      name: '',
+      address: ''
+    }); }
+    ngOnInit() {
+      this.items = this.cartService.getItems();
+    }
+      onSubmit(customerData) {
+        // Process checkout data here
+        console.warn('Your order has been submitted', customerData);
+        this.items = this.cartService.clearCart();
+        this.checkoutForm.reset(alert('thank you for your purchase'));
+      }
+      plus(i: string | number) {
+        this.items[i].quantity++;
+        this.items[i].total = this.items[i].quantity * this.items[i].price;
+    }
+      minus(i: string | number) {
+        if (this.items[i].quantity === 1) {
+          this.items[i].quantity = 0;
+          this.items.splice(i, 1);
+        } else {
+          this.items[i].quantity--;
+        }
+      }
+      removeItem(i: string | number) {
+        this.items[i].quantity = 0;
+        this.items.splice(i, 1 );
+      }
+      total() {
+        let total = 0;
+        for (let i of this.items) {
+          total += i.total;
+          console.log(total);
+        }
+      }
+  }
+*/
